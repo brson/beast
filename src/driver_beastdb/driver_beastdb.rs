b323afa20e22e7541a13557f33e88dce3c5bc20c
Brian Anderson <andersrb@gmail.com>
2020-01-11 12:46:13 +0800

    Outline beastdb driver

--- src/driver_beastdb/driver_beastdb.rs
+++ src/driver_beastdb/driver_beastdb.rs
@@ -1,10 +1,34 @@
+#![allow(unused)]
+
 use b_error::BResult;
 
 pub fn beastdb_server_run(args: &[String]) -> BResult<i32> {
-    eprintln!("Hi. I'm Beast, the database. I do nothing yet.");
-    eprintln!("Try running one of my personas:");
-    eprintln!("    beastdb +tidb-server");
-    eprintln!("    beastdb +pd-server");
-    eprintln!("    beastdb +tikv-server");
-    Ok(1)
+    b_error::main(run);
+
+    Ok(0)
+}
+
+fn run() -> BResult<()> {
+    run_pd()?;
+    run_tikv()?;
+    run_tidb()?;
+    wait_for_shutdown()?;
+
+    Ok(())
+}
+
+fn run_pd() -> BResult<()> {
+    panic!()
+}
+
+fn run_tikv() -> BResult<()> {
+    panic!()
+}
+
+fn run_tidb() -> BResult<()> {
+    panic!()
+}
+
+fn wait_for_shutdown() -> BResult<()> {
+    panic!()
 }
